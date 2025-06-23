d2e0bf82c17d4eae0c675276e91509d7915c542a
Gaurav Singh <gaurav.singh.971999@gmail.com>
2025-06-23 21:19:58 +0530

    Create pq1.js

---
+++ Web Detla/Day14/pq1.js
@@ -0,0 +1,17 @@
+//System to calculate popcorn prices base on the size customer asked for
+let size = "XL";
+if (size === "XL") {
+    console.log("Size is XL : price is 250");
+}
+else if (size === "L") {
+    console.log("Size is L: price is 200");
+}
+else if(size === "M") {
+    console.log("Size is M: price is 100");
+}
+else if(size === "S") {
+    console.log("Size is S: price is 50");
+}
+else{
+    console.log("not available");
+}
