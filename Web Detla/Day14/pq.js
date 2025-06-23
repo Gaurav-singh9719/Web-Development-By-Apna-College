9c4d2db41a37806000fc0fddf3e1160e1245239a
Gaurav Singh <gaurav.singh.971999@gmail.com>
2025-06-23 21:18:13 +0530

    Create pq.js

---
+++ Web Detla/Day14/pq.js
@@ -0,0 +1,18 @@
+let color = 'red';
+if(color === "red") {
+    console.log("stop! light color is red");
+}
+if(color === "yellow") {
+    console.log("slow down. light color is yellow");
+}
+if (color === "green") {
+    console.log("Go. light color is green");
+}
+
+let age = 14;
+if(age >= 18) {
+    console.log("you can voat");
+}
+else if(age < 18) {
+    console.log("you can not voat");
+}
