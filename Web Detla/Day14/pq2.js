9b23dab0a60be9f8d1ef8f6e71305244b9a13bd0
Gaurav Singh <gaurav.singh.971999@gmail.com>
2025-06-23 21:21:04 +0530

    Create pq2.js

---
+++ Web Detla/Day14/pq2.js
@@ -0,0 +1,22 @@
+let str = "apple";
+if (str[0] === "a" && str.length > 3) {
+    console.log("Good String");
+}
+else {
+    console.log("not a good String");
+}
+let color = "red";
+switch (color) {
+    case "red":
+        console.log("Stop");
+        break;
+    case "yellow":
+        console.log("Slow Down");
+        break;
+    case "green":
+        console.log("Go");
+        break;
+    default:
+        console.log("Broken light");
+        break;
+}
