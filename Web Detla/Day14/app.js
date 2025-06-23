6964a474c523bc41c4163cae09ee1f5b251903cf
Gaurav Singh <gaurav.singh.971999@gmail.com>
2025-06-23 21:17:17 +0530

    Create app.js

---
+++ Web Detla/Day14/app.js
@@ -0,0 +1,40 @@
+console.log("Hello World");
+console.log("Apna College");
+let a = 10;
+let b = 20;
+console.log("Sum is :", a+b);//this is a comment 
+let pencilPrice = 10;
+let eraserPrice = 5;
+// let output= "the total price is :" + (pencilPrice + eraserPrice) + " Rupees" ;
+let output = `the total price is : ${pencilPrice+eraserPrice} Rupees`; //back ticks ``
+console.log(output);
+console.log(a+b);
+console.log(a-b)
+console.log(a*b);
+console.log(a/b);
+console.log(a%b)
+console.log(a**b);
+console.log(a++);
+console.log(a);
+console.log(++a);
+b=a;
+console.log(b);  
+let age = 18;
+console.log(age!=18);
+let n =5;
+let str = '5';
+typeof n;
+typeof str;
+console.log(n == str);
+console.log("Before my if Statement");
+let age1 = 23;
+if(age1>=18){
+    console.log("you can voat");
+    console.log("you can drive");
+}
+
+console.log("After my if statement");
+let name = "gaurav";
+if(name=="gaurav"){
+    console.log(`Welcome ${name}`);
+}
